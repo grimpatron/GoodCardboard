6a1755562e7e13b40236f28604faa52a1f9dde58
grimpatron <Hellhammer220bpm@gmail.com>
2023-08-26 15:45:36 +0300

    feat: make other sections, change images and add favicon

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+  $('.header-contacts__button').on('click', function(){
+    $('.overlay').show();
+  });
+  $('.popup-close').on('click', function(){
+    $('.overlay').hide();
+  })
+});
